ae3f6deda013f8642cb337b876f115125353e34c
yixiu <yixiu@gaoding.com>
2022-12-22 16:01:47 +0800

    types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,34 @@
-export function signature(basis_file_path: string, sig_file_path: string);
-export function delta(sig_file_path: string, new_file_path: string, delta_file_path: string);
-export function patch(basis_file_path: string, delta_file_path: string, new_file_path: string);
+export function signature(basis_file_path: string, sig_file_path: string): void;
+export function delta(
+  sig_file_path: string,
+  new_file_path: string,
+  delta_file_path: string
+): void;
+export function patch(
+  basis_file_path: string,
+  delta_file_path: string,
+  new_file_path: string
+): void;
+export function signatureAsync(
+  basis_file_path: string,
+  sig_file_path: string
+): Promise<void>;
+export function deltaAsync(
+  sig_file_path: string,
+  new_file_path: string,
+  delta_file_path: string
+): Promise<void>;
+export function patchAsync(
+  basis_file_path: string,
+  delta_file_path: string,
+  new_file_path: string
+): Promise<void>;
 declare const rdiff: {
-    signature,
-    delta,
-    patch,
-}
+  signature;
+  delta;
+  patch;
+  signatureAsync;
+  deltaAsync;
+  patchAsync;
+};
 export default rdiff;
